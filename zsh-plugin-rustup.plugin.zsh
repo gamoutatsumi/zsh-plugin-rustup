9f1f8597f7b618ef00454566345edfd0c491105a
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-04-20 12:18:24 +0900

    update

--- zsh-plugin-rustup.plugin.zsh
+++ zsh-plugin-rustup.plugin.zsh
@@ -1,1 +1,2 @@
 rustup completions zsh > ${$(readlink -f $0):h}/_${$(readlink -f $0):h:t}
+ln -s $HOME/.rustup/toolchains/$(rustup default 2>&1 | awk '{ print $1 }')/share/zsh/site-functions/_cargo ./cargo
